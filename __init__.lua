ba01b3225a14f0d23bb292a282d4555fb4d30944
BLANKxSS <35851517+BLANKxSS@users.noreply.github.com>
2022-06-28 10:15:18 +0300

    init

--- __init__.lua
+++ __init__.lua
@@ -1,40 +1,45 @@
--- modified version of https://github.com/Joelrau/S1x-IW6x-g_log-script (permission to use by author)
 
 if (game:getdvar("gamemode") ~= "mp") then
     return
 end
 
--- function player_damage(self_, inflictor, attacker, damage, dflags, mod, weapon, vPoint, vDir, hitLoc)
---     self_:println("s")
--- end
-
 level:onnotify("player_spawned", function(player)
     setSnip(player)
+    removePerks(player)
 end)
 
--- level:onnotify("changed_class",function(player) 
---     player:clientiprintln(player.name)
--- end)
--- make sure this function invokes to the original if you're not modifying anything - you want the original logic to still run
-local change_class = nil
-
+function removePerks(player)
+    player:unsetperk("specialty_pistoldeath" , false)
+    player:unsetperk("specialty_grenadepulldeath" , false)
+    player:unsetperk("specialty_armorvest" , false)    
+end
+function randomCamo()
+     camos = {"camo010","camo011","camo012","camo013","camo014","camo015"}
+     return camos[math.random( 0, 5 )]
+end
 
 function setSnip(player)
-    player:unsetperk("specialty_pistoldeath" , true)
-    player:unsetperk("specialty_grenadepulldeath" , true)
-    player:unsetperk("specialty_armorvest" , true)
+    local change_class = nil
+    
     change_class = game:detour("maps/mp/gametypes/_menus", "_id_5BB2", function(self_, var0)
         -- change class event here!
         change_class.invoke(self_, var0)
-        setSnip(self_)
+        -- if player:getcurrentweapon() ~= "h1_m40a3_mp_a#none_f#base" then
+        applySnip(player)
     end)
-    if player:getcurrentweapon() ~= "h1_m40a3_mp_a#none_f#base" then
+    applySnip(player)
+end
+
+function applySnip(player)
+    currCamo = tostring(randomCamo())
+    game:ontimeout(function()
         player:takeallweapons()
-        player:giveweapon("h1_m40a3_mp_a#none_f#base")
-        player:giveweapon("h1_m40a3_mp_a#acog_f#base")
-        game:ontimeout(function()
-        player:switchtoweapon("h1_m40a3_mp_a#none_f#base")
-        end, 100)
-      end
+        removePerks(player)
+        player:giveweapon("h1_m40a3_mp_a#none_f#base_"..currCamo.."")
+        player:giveweapon("h1_m40a3_mp_a#acog_f#base_"..currCamo.."")
+        player:clientiprintln("not ^1allowed "..currCamo.."")
+        
+        player:switchtoweapon("h1_m40a3_mp_a#none_f#base_"..currCamo.."")
+        end, 500)
 end
 -- init()
